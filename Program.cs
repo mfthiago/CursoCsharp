af955eef75f90935b4d9f49a35cd2f92c3560036
Thiago Mello <mfthiagod@gmail.com>
2024-03-25 12:39:24 -0300

    exercício de fixação

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercícioInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.WriteLine("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Date: (dd/MM/yyyy");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Contract Value: ");
+            double value = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of installments: ");
+            int installment = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Services/ContractServices.cs
@@ -0,0 +1,28 @@
+﻿using ExercícioInterface.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioInterface.Services
+{
+    internal class ContractServices
+    {
+        IOnlinePaymentService _onlinePaymentService;
+
+        public ContractServices(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProccessContract(Contract contract, int months)
+        {
+            double basicQuoota
+
+        
+
+        }
+
+    }
+}
