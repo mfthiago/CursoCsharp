fb8a542e597d61ecc0dcf620e2a4770ee7a1167d
Thiago Mello <mfthiagod@gmail.com>
2024-03-26 21:06:37 -0300

    Conjuntos

---
+++ Conjuntos/Entities/LogRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conjuntos.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+
+    }
+}
