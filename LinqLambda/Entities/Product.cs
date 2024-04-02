e86b9fd75adc321eadcdff118e4de61f4b18b3eb
Thiago Mello <mfthiagod@gmail.com>
2024-04-02 10:47:23 -0300

    Linq Lambda

---
+++ LinqLambda/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLambda.Entities
+{
+    internal class Product
+    {
+    }
+}
